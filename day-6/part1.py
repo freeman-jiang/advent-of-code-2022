d5a6c6ec083ce93651ca0034e309b20225a0577e
Freeman Jiang <freeman.jiang.ca@gmail.com>
2022-12-07 23:24:05 -0500

    Day 6

---
+++ day-6/part1.py
@@ -0,0 +1,18 @@
+WINDOW_SIZE = 4
+
+
+def main():
+    with open("input.txt") as file:
+        datastream = file.read().strip()
+
+        for i, c in enumerate(datastream):
+            window = datastream[i:i + WINDOW_SIZE]
+            # print(window)
+            if len(window) == len(set(window)):
+                answer = i + WINDOW_SIZE
+                print(answer)
+                return
+
+
+if __name__ == "__main__":
+    main()
